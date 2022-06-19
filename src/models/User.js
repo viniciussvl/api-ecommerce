c14b5c8c1ae8376f55d5f817d2f680511ca89298
Vinicius Aquino <viniciussvl@hotmail.com>
2022-06-19 19:18:44 -0300

    Add User.js model mongoose

---
+++ src/models/User.js
@@ -0,0 +1,27 @@
+const mongoose = require('mongoose');
+
+const UserSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        require: true
+    },
+    email: {
+        type: String,
+        require: true,
+        unique: true
+    },
+    password: {
+        type: String,
+        require: true,
+        select: false
+    },
+    timestamps: true
+})
+
+UserSchema.pre('save', function(next) {
+    // faça alguma coisa em algum campo antes de SALVAR
+})
+
+const User = mongoose.model('User', UserSchema);
+
+module.exports = User;
