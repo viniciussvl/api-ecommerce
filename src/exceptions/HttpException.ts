71833da428d675e7760d26cec769db34d1b0b7df
Vinicius Aquino <viniciussvl@hotmail.com>
2022-06-27 17:13:09 -0300

    Documentando exceção HttpException e controllers Register e Login

--- src/exceptions/HttpException.ts
+++ src/exceptions/HttpException.ts
@@ -1,3 +1,12 @@
+/** This class is an exception that inherits the Error interface 
+ * to insert an additional parameter into the exception's return object.
+ * 
+ * @param {number} status a status code
+ * @param {message} string exception message
+ * @returns 
+ * Include status code in Error Exception
+ */
+
 class HttpException extends Error {
     public status: number;
     public message: string;
