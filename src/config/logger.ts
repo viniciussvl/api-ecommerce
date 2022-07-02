3ab07b0b4a54fe8274daf702564710a4cdf811c3
Vinicius Aquino <viniciussvl@hotmail.com>
2022-07-02 15:10:10 -0300

    Add winston logger para gerar logs dentro da aplicaçao, configurando arquivo logger.ts

---
+++ src/config/logger.ts
@@ -0,0 +1,35 @@
+import winston from 'winston';
+import 'winston-daily-rotate-file';
+
+const transport = new winston.transports.DailyRotateFile({
+  filename: 'logs/node-%DATE%.log',
+  datePattern: 'YYYY-MM-DD',
+  zippedArchive: true,
+  maxSize: '20m',
+  maxFiles: '14d',
+});
+
+transport.on('rotate', function (oldFilename, newFilename) {
+  // do something fun
+});
+
+const logger = winston.createLogger({
+  level: 'info',
+  format: winston.format.json(),
+  defaultMeta: { service: 'user-service' },
+  transports: [transport],
+});
+
+//
+// If we're not in production then log to the `console` with the format:
+// `${info.level}: ${info.message} JSON.stringify({ ...rest }) `
+//
+if (process.env.NODE_ENV !== 'production') {
+  logger.add(
+    new winston.transports.Console({
+      format: winston.format.simple(),
+    })
+  );
+}
+
+export default logger;
--- src/controllers/ProductController.ts
+++ src/controllers/ProductController.ts
@@ -1,7 +1,8 @@
-import { Request, Response } from "express";
-import IQueryParams from "../common/interfaces/IQueryParams";
-import ProductDto from "../dto/ProductDto";
-import ProductService from "../services/ProductService";
+import { Request, Response } from 'express';
+import IQueryParams from '../common/interfaces/IQueryParams';
+import ProductDto from '../dto/ProductDto';
+import ProductService from '../services/ProductService';
+import logger from '../config/logger';
 
 class ProductController {
   private productService: ProductService;
@@ -15,8 +16,10 @@ class ProductController {
 
     try {
       const products = await this.productService.getProducts(queryParams);
+      logger.info('Listando produtos via API');
       res.status(200).json(products);
     } catch (error: any) {
+      logger.error(error);
       res.status(error.status).json({ error: error.message });
     }
   }
@@ -38,7 +41,7 @@ class ProductController {
       const product = await this.productService.createProduct(data);
       res
         .status(200)
-        .json({ message: "Product created successfully", product: product });
+        .json({ message: 'Product created successfully', product: product });
     } catch (error: any) {
       res.status(error.status).json({ error: error.message });
     }
@@ -50,7 +53,7 @@ class ProductController {
 
     try {
       await this.productService.updateProduct(id, data);
-      res.status(201).json({ message: "Product updated", product: data });
+      res.status(201).json({ message: 'Product updated', product: data });
     } catch (error: any) {
       res.status(error.status).json({ error: error.message });
     }
